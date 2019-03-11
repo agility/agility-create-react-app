0ff1c2a21f1cea9e65a0b23781fc9d577fda436b
James Vidler <james.vidler@outlook.com>
2019-03-11 16:49:50 -0400

    added test calls for fetch api

--- src/App.js
+++ src/App.js
@@ -6,14 +6,53 @@ import agility from 'agility-js-sdk'
 class App extends Component {
   componentDidMount() {
     
-    var client = agility.createClient({
+    var api = agility.getApi({
       instanceID: '1234-1234',
       accessToken: 'fEpTcRnWO3EahHbojDCeY3PwGwAzpw2gveDuPn2l0nuqFbQYVcWrQ+a3/DHcWgCgn7UL2tgbSOS0AqrEOiXkTg==',
       languageCode: 'en-us'
     })
 
-    client.getSitemapFlat();
+    api.getSitemapFlat()
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(response) {
+          console.log(response);
+      });
 
+      api.getSitemapNested()
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(response) {
+          console.log(response);
+      });
+
+      api.getContentItem(22)
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(response) {
+          console.log(response);
+      });
+
+      api.getContentList("posts")
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(response) {
+          console.log(response);
+      });
+
+      api.getPage(1)
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(response) {
+          console.log(response);
+      });
+
+      
   }
   render() {    
 
